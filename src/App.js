bbfbb900ef702128fa4aa8e560773b0ad244ef29
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-07-08 19:34:42 +0700

    add font

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./assets/css/styles.css";
 
 function App() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto font-poppins">
       <h1 className="text-5xl text-blue-700 font-black">Hello, React</h1>
     </div>
   );
